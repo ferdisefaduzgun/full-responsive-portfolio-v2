40d3025f23e1d560b33bb09e93d5aa7fc8cea682
Ferdi Sefa Düzgün <76045185+ferdisefaduzgun@users.noreply.github.com>
2023-05-20 14:25:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.body.classList.toggle("darkmode");
+})
+
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('b').classList.toggle("darkmodee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('z').classList.toggle("darkmodeee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('l').classList.toggle("darkmodeee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('x').classList.toggle("darkmodeee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('k').classList.toggle("darkmodeee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('j').classList.toggle("darkmodeee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('u').classList.toggle("darkmodeeee");
+})
